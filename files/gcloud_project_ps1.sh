de5c2aa48d497ee8d4afd616c6e68771bf533003
root <mjdryden@gmail.com>
2020-04-01 20:39:29 +0000

    no project selected support for gcloud prompt

--- files/gcloud_project_ps1.sh
+++ files/gcloud_project_ps1.sh
@@ -1,6 +1,12 @@
 __gcloud_project_ps1() {
-    if test -f "/root/.config/gcloud/active_config"; then
-        CONTEXT=$(cat /root/.config/gcloud/active_config)
+    if test -f "/root/.config/gcloud"; then
+        if test -f "/root/.config/gcloud/active_config"; then
+            CONTEXT=$(cat /root/.config/gcloud/active_config)
+        fi
+
+        if [ -z "$CONTEXT" ]; then
+            CONTEXT="(none)"
+        fi
 
         if [ -n "$CONTEXT" ]; then
             echo -e "${CYAN}gcp:${CONTEXT} "
