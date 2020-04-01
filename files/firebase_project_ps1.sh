98e3560c7206410b2fb18e27802e3a1ad7f8877a
root <mjdryden@gmail.com>
2020-04-01 20:16:21 +0000

    Improve active project handling

--- files/firebase_project_ps1.sh
+++ files/firebase_project_ps1.sh
@@ -1,7 +1,5 @@
 __firebase_project_ps1() {
-    if test -f "/root/.config/configstore/firebase-tools.json"; then
-        CONTEXT=$(grep \"$(pwd)\" ~/.config/configstore/firebase-tools.json | cut -d" " -f2 | cut -d"\"" -f2)
-    fi
+    CONTEXT=$(__firebase_active_project)
        
     if [ -z "$CONTEXT" ]; then
         CONTEXT=$(__firebase_default)
@@ -14,6 +12,18 @@ __firebase_project_ps1() {
     echo -e "${LIME_YELLOW}fb:$CONTEXT "
 }
 
+__firebase_active_project() {
+    if test -f "/root/.config/configstore/firebase-tools.json"; then
+        CONTEXT=$(grep \"$(pwd)\" ~/.config/configstore/firebase-tools.json | cut -d" " -f2 | cut -d"\"" -f2)
+
+        if [ -z "$CONTEXT" ]; then
+            CONTEXT=$(grep \"$(dirname $(pwd))\" ~/.config/configstore/firebase-tools.json | cut -d" " -f2 | cut -d"\"" -f2)
+        fi
+
+        echo -e "$CONTEXT"
+    fi
+}
+
 __firebase_default() {
     if test -f "$(pwd)/.firebaserc"; then
         echo -e "$(__firebase_default_from $(pwd)/.firebaserc)"
@@ -21,10 +31,6 @@ __firebase_default() {
     
     if test -f "$(pwd)/../.firebaserc"; then
         echo -e "$(__firebase_default_from $(pwd)/../.firebaserc)"
-    fi
-    
-    if test -f "$(pwd)/../../.firebaserc"; then
-        echo -e "$(__firebase_default_from $(pwd)/../../.firebaserc)"
     fi
 }
 
