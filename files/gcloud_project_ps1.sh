d78a7d8155965653197be82b1b235a5567b03eaa
root <mjdryden@gmail.com>
2020-04-01 20:44:59 +0000

    fix for gcloud detection

--- files/gcloud_project_ps1.sh
+++ files/gcloud_project_ps1.sh
@@ -1,15 +1,11 @@
 __gcloud_project_ps1() {
-    if test -f "/root/.config/gcloud"; then
+    if test -d "/root/.config/gcloud"; then
+        CONTEXT="(none)"
+
         if test -f "/root/.config/gcloud/active_config"; then
             CONTEXT=$(cat /root/.config/gcloud/active_config)
         fi
 
-        if [ -z "$CONTEXT" ]; then
-            CONTEXT="(none)"
-        fi
-
-        if [ -n "$CONTEXT" ]; then
-            echo -e "${CYAN}gcp:${CONTEXT} "
-        fi
+        echo -e "${CYAN}gcp:${CONTEXT} "
     fi
 }
